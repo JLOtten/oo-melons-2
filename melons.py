d01fdb4113a2fd4c1710ed53b8db67d8cdf50430
Jenna Otten <jenna.otten@gmail.com>
2022-09-30 13:06:15 -0700

    Modified lab

--- melons.py
+++ melons.py
@@ -8,25 +8,27 @@ class AbstractMelonOrder:
         self.species = species
         self.qty = qty
 
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        if self.species == "christmas melon":
+            base_price = base_price * 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
-
-        #self.species = species
-        #self.qty = qty
+        super().__init__(species, qty)
         self.shipped = False
         self.order_type = "domestic"
         self.tax = 0.08
 
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
@@ -41,9 +43,8 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
-
-        #self.species = species
-        #self.qty = qty
+        super().__init__(species, qty)
+        
         self.country_code = country_code
         self.shipped = False
         self.order_type = "international"
@@ -57,11 +58,12 @@ class InternationalMelonOrder(AbstractMelonOrder):
             base_price = base_price * 1.5
 
         total = (1 + self.tax) * self.qty * base_price
+
         if self.qty < 10:
             total += 3
 
         return total
-
+        
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
@@ -73,10 +75,18 @@ class InternationalMelonOrder(AbstractMelonOrder):
         return self.country_code
 
 class GovernmentMelonOrder(AbstractMelonOrder):
-    passed_inspection = False
+    
+    """A melon order purchased by the US Government."""
 
-    def __init__(self, passed):
-        self.passed = passed
+    order_type = "government"
+    tax = 0.0
 
-    def mark_inspection(passed):
-        passed_inspection = True
+    def __init__(self, species, qty):
+        super().__init__(species, qty)
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        """Record whether the melon order has passed inspection or not."""
+
+        self.passed_inspection = passed
+   
